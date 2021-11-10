6efa9c3642131a9bbcf0203186291a30f9798e3f
Lean9090P <76073574+Lean9090P@users.noreply.github.com>
2021-11-10 10:55:08 -0300

    Add files via upload

---
+++ Flete.java
@@ -0,0 +1,12 @@
+package Empresa_de_transporte;
+
+public class Flete {
+	private int _codigoFlete;
+	private String _descripcion;
+	public Camion _unnamed_Camion_;
+	public Producto _unnamed_Producto_;
+
+
+
+
+}
